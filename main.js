aea75bd228dea17187219a6436d3b8a0b7565857
jojomoore2007 <31520139+jojomoore2007@users.noreply.github.com>
2022-05-20 14:43:10 -0400

    Update main.js

--- main.js
+++ main.js
@@ -13,39 +13,38 @@ try {
       var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
       var analyser = audioCtx.createAnalyser();
       var source = audioCtx.createMediaStreamSource(stream);
-      analyser.fftSize = 512;
+      analyser.fftSize = 256;
       var bufferLength = analyser.frequencyBinCount;
       var streamFrames = 64;
       var fft = [];
       for (let i = 0; i < streamFrames; i++) {
-        fft.push(new Uint8Array(bufferLength));
+        fft[i]=new Uint8Array(bufferLength);
       }
       source.connect(analyser);
       canvas.clearRect(0,0,canvasElem.width,canvasElem.height);
       n=0
       function draw() {
-        let WIDTH = window.innerWidth, HEIGHT = window.innerHeight;
-        canvasElem.width=WIDTH;
-        canvasElem.height=HEIGHT;
-        analyser.getByteFrequencyData(fft[n]);
-        n=(n+1)%streamFrames;
-        let pxWidth = 1+(WIDTH/streamFrames);
-        let pxHeight = 1+(HEIGHT/bufferLength);
-        for (let x = 0; x < streamFrames; x++) {
-          for (let y = 0; y < bufferLength; y++) {
-            let c = (fft[(n+x)%streamFrames][y]-fft[(n+x-1)%streamFrames][y]);
-            canvas.fillStyle = "#"+(((((0x100|c)<<8)|c)<<8)|c).toString(16).substring(1);
-            canvas.fillRect(x*pxWidth,y*pxHeight,pxWidth,pxHeight);
+        try {
+          let WIDTH = window.innerWidth, HEIGHT = window.innerHeight;
+          canvasElem.width=WIDTH;
+          canvasElem.height=HEIGHT;
+          analyser.getByteFrequencyData(fft[n]);
+          n=(n+1)%streamFrames;
+          let pxWidth = (WIDTH/streamFrames)*2;
+          let pxHeight = (HEIGHT/bufferLength)*2;
+          for (let x = 0; x < streamFrames; x++) {
+            for (let y = 0; y < bufferLength; y++) {
+              let c = fft[(n+x)%streamFrames][y];
+              canvas.fillStyle = "#"+(((0x010101*c)|0x1000000).toString(16).substring(1));
+              canvas.fillRect(x*pxWidth,y*pxHeight,pxWidth,pxHeight);
+            }
           }
-        }
-        var drawVisual = requestAnimationFrame(draw);
+          var drawVisual = requestAnimationFrame(draw);
+        } catch (e) {alert(e.toString());}
       }
       draw()
     });
   } else {
     canvasElem.innerText = "Your browser does not support the canvas element and cannot run this app. Please update your browser!";
   }
-} catch (e) {
-  errDiv.innerText += "Error:\n"+e.toString()+"\n";
-  document.body.appendChild(errDiv);
-}
+} catch (e) {alert(e.toString());}
