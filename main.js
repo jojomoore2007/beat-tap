0ca4f193b0f33dc2ad551a0c6f10ad6a5e3be825
jojomoore2007 <31520139+jojomoore2007@users.noreply.github.com>
2022-05-20 14:13:29 -0400

    Update main.js

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ try {
       var streamFrames = 64;
       var fft = [];
       for (let i = 0; i < streamFrames; i++) {
-        fft.push(new Uint8Array(bufferLength));
+        fft.push(new Float32Array(bufferLength));
       }
       source.connect(analyser);
       canvas.clearRect(0,0,canvasElem.width,canvasElem.height);
@@ -29,14 +29,14 @@ try {
           canvasElem.width=WIDTH;
           canvasElem.height=HEIGHT;
         }
-        analyser.getByteFrequencyData(fft[n]);
+        analyser.getFloatFrequencyData(fft[n]);
         n=(n+1)%streamFrames;
         var drawVisual = requestAnimationFrame(draw);
         let pxWidth = WIDTH/streamFrames;
         let pxHeight = HEIGHT/bufferLength;
         for (let x = 0; x < streamFrames; x++) {
           for (let y = 0; y < bufferLength; y++) {
-            let c = fft[(n+x)%streamFrames][y];
+            let c = Math.floor(10**((fft[(n+x)%streamFrames][y])/10));
             canvas.fillStyle = "#"+(((((0x100|c)<<8)|c)<<8)|c).toString(16).substring(1);
             canvas.fillRect(x*pxWidth,y*pxHeight,pxWidth,pxHeight);
           }
