08dc602b1fb15c2022e3df3a020d93aadaeea9c4
jojomoore2007 <31520139+jojomoore2007@users.noreply.github.com>
2022-05-20 14:37:11 -0400

    Update main.js

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ try {
       var analyser = audioCtx.createAnalyser();
       var source = audioCtx.createMediaStreamSource(stream);
       analyser.fftSize = 512;
-      var bufferLength = Math.floor(analyser.frequencyBinCount/2);
+      var bufferLength = analyser.frequencyBinCount;
       var streamFrames = 64;
       var fft = [];
       for (let i = 0; i < streamFrames; i++) {
@@ -25,10 +25,8 @@ try {
       n=0
       function draw() {
         let WIDTH = window.innerWidth, HEIGHT = window.innerHeight;
-        if ((WIDTH!=canvasElem.width)||(HEIGHT!=canvasElem.height)) {
-          canvasElem.width=WIDTH;
-          canvasElem.height=HEIGHT;
-        }
+        canvasElem.width=WIDTH;
+        canvasElem.height=HEIGHT;
         analyser.getByteFrequencyData(fft[n]);
         n=(n+1)%streamFrames;
         let pxWidth = 1+(WIDTH/streamFrames);
@@ -36,7 +34,7 @@ try {
         for (let x = 0; x < streamFrames; x++) {
           for (let y = 0; y < bufferLength; y++) {
             let c = (fft[(n+x)%streamFrames][y]-fft[(n+x-1)%streamFrames][y]);
-            canvas.fillStyle = "#"+(((((0x100|Math.max(0,c))<<8)|(Math.abs(c)>>1))<<8)|Math.max(0,-c)).toString(16).substring(1);
+            canvas.fillStyle = "#"+(((((0x100|c)<<8)|c)<<8)|c).toString(16).substring(1);
             canvas.fillRect(x*pxWidth,y*pxHeight,pxWidth,pxHeight);
           }
         }
