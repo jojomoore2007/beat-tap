dcb28e405e16304eea1c4f314489d522ec76710d
jojomoore2007 <31520139+jojomoore2007@users.noreply.github.com>
2022-05-20 14:09:01 -0400

    Update main.js

--- main.js
+++ main.js
@@ -18,28 +18,29 @@ try {
       var streamFrames = 64;
       var fft = [];
       for (let i = 0; i < streamFrames; i++) {
-        fft[i]=new Uint8Array(bufferLength);
+        fft.push(new Uint8Array(bufferLength));
       }
       source.connect(analyser);
       canvas.clearRect(0,0,canvasElem.width,canvasElem.height);
       n=0
       function draw() {
         let WIDTH = window.innerWidth, HEIGHT = window.innerHeight;
-        canvasElem.width=WIDTH;
-        canvasElem.height=HEIGHT;
+        if ((WIDTH!=canvasElem.width)||(HEIGHT!=canvasElem.height)) {
+          canvasElem.width=WIDTH;
+          canvasElem.height=HEIGHT;
+        }
         analyser.getByteFrequencyData(fft[n]);
         n=(n+1)%streamFrames;
-        var pxWidth = WIDTH/streamFrames;
-        var pxHeight = HEIGHT/bufferLength;
-        let c = 0;
+        var drawVisual = requestAnimationFrame(draw);
+        let pxWidth = WIDTH/streamFrames;
+        let pxHeight = HEIGHT/bufferLength;
         for (let x = 0; x < streamFrames; x++) {
           for (let y = 0; y < bufferLength; y++) {
-            c = fft[(n+x)%streamFrames][y];
+            let c = fft[(n+x)%streamFrames][y];
             canvas.fillStyle = "#"+(((((0x100|c)<<8)|c)<<8)|c).toString(16).substring(1);
             canvas.fillRect(x*pxWidth,y*pxHeight,pxWidth,pxHeight);
           }
         }
-        var drawVisual = requestAnimationFrame(draw);
       }
       draw()
     });
