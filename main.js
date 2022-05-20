e494f84dd41c1827a1144cd96b4c79cd6156a2eb
jojomoore2007 <31520139+jojomoore2007@users.noreply.github.com>
2022-05-20 15:36:06 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 class Runtime {
   constructor() {
-    this.canvasElem = window.document.createElement("canvas");
-    this.errDiv = window.document.createElement("div");
+    this.canvasElem = document.createElement("canvas");
+    this.errDiv = document.createElement("div");
     if (this.canvasElem.getContext) {
-      this.works = true
+      this.works = true;
       this.canvasElem.style.position="absolute";
       this.canvasElem.style.top=0;
       this.canvasElem.style.left=0;
     }
-    this.colors = []
+    this.colors = [];
     for (let i = 0; i < 256; i++) {
-      this.colors[i] = "#"+(((0x180|(i>>1))<<16)|(0xff^(i>>1))).toString(16).substring(1);
+      this.colors.push("#"+(((0x180|(i>>1))<<16)|(0xff^(i>>1))).toString(16).substring(1));
     }
   }
   draw() {
@@ -34,7 +34,7 @@ class Runtime {
     this.analyser = this.audioCtx.createAnalyser();
     this.source = this.audioCtx.createMediaStreamSource(stream);
     this.analyser.fftSize = 2048;
-    this.bufferLength = analyser.frequencyBinCount;
+    this.bufferLength = this.analyser.frequencyBinCount;
     this.frames = 256;
     this.fft = [];
     for (let i = 0; i < this.frames; i++) {
@@ -42,27 +42,22 @@ class Runtime {
     }
     this.source.connect(this.analyser);
     this.canvas.clearRect(0,0,this.canvasElem.width,this.canvasElem.height);
-    n=0
-    function draw() {
-      try {
-        
-      } catch (e) {alert(e.toString());}
-    }
-    draw()
+    this.n=0;
+    this.draw();
   }
   begin() {
     try {
       if (this.works) {
         this.canvasElem.width=window.innerWidth;
         this.canvasElem.height=window.innerHeight;
-        window.document.body.appendChild(this.canvasElem);
+        document.body.appendChild(this.canvasElem);
         this.canvas = this.canvasElem.getContext('2d');
-        window.navigator.mediaDevices.getUserMedia({video: false, audio: true}).then(stream=>{
-          
-        });
+        window.navigator.mediaDevices.getUserMedia({video: false, audio: true}).then(this.graph);
       } else {
-        canvasElem.innerText = "Your browser does not support the canvas element and cannot run this app. Please update your browser!";
+        this.canvasElem.innerText = "Your browser does not support the canvas element and cannot run this app. Please update your browser!";
+        document.body.appendChild(this.canvasElem);
       }
-    } catch (e) {alert(e.toString());}
+    } catch (e) {window.alert(e.toString());}
   }
 }
+window.mainClass = Runtime();
